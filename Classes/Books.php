4cac7d027582f1b425a133a15f866501dd38ba8f
Serg Lesyuk <seranatles@gmail.com>
2024-01-25 12:33:59 +0200

    * book list add genre

--- Classes/Books.php
+++ Classes/Books.php
@@ -8,5 +8,14 @@
         protected string $table = 'books';
         protected string $key = 'id';
 
-        //todo data validation
+        public function getList($count = 10, $page = 1, $order_by = 'name'): array
+        {
+            $page--;
+            $sql = "SELECT b.*, g.name genre FROM books b
+                        LEFT JOIN genres g ON g.id = b.genre_id
+                        ORDER BY $order_by 
+                        LIMIT $count OFFSET " . ($page * $count);
+
+            return $this->db->fetchAll($sql);
+        }
     }
